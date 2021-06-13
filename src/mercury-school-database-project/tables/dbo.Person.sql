1c29f726fa370b1314adc8a293c330c7877aa295
Michael Henderson <mike@mohenderson.com>
2021-06-13 11:33:36 -0400

    Added sequence.

--- src/mercury-school-database-project/tables/dbo.Person.sql
+++ src/mercury-school-database-project/tables/dbo.Person.sql
@@ -1,6 +1,8 @@
-CREATE TABLE [dbo].[dbo.Person]
+CREATE TABLE dbo.Person
 (
-  Id int NOT NULL CONSTRAINT PK_Person PRIMARY KEY,
+  Id int NOT NULL
+    CONSTRAINT PK_Person PRIMARY KEY
+    CONSTRAINT DF_Person_Id DEFAULT NEXT VALUE FOR dbo.PersonSequence,
   FirstName nvarchar(128) NOT NULL,
   MiddleName nvarchar(128) NULL,
   LastName nvarchar(128) NOT NULL,
